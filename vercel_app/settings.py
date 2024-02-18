454baeaa7b6b3a134ca9626a7ac91caeb7e244b6
zzaizzai <junsai0530@gmail.com>
2024-02-18 22:44:22 +0900

    99

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -27,7 +27,7 @@ load_dotenv(dotenv_path)
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
@@ -129,12 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static",
-STATICFILES_DIRS = [
-    BASE_DIR / "static/img",
-]
+STATIC_URL = 'staic/'
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static")
+    ]
+
+STATIC_ROOT = BASE_DIR / "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
