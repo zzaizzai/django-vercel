a183b19a79587a0c4185aac2640f91d3ead02390
zzaizzai <junsai0530@gmail.com>
2024-02-18 21:19:50 +0900

    static_root

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
