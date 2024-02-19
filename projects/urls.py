ff98df9c17fd27fa8c932fd91b36b942a753f288
zzaizzai <junsai0530@gmail.com>
2024-02-19 19:36:48 +0900

    11

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,7 @@
 # example/urls.py
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import *
 
@@ -10,3 +12,5 @@ urlpatterns = [
     path('list',  ProjectList.as_view(), name='project_list'),
     path('create_table', create_table, name='create_table_project'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
