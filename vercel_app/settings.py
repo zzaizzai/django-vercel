1b64033c9feb746e5e127ab12e3e842c5e4965f1
zzaizzai <junsai0530@gmail.com>
2024-02-25 00:51:23 +0900

    config

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 STATIC_URL = '/static/'
 
